a619517805322656c473a24bf966b95358c4a777
poy410 <poy410@naver.com>
2016-05-10 17:54:58 +0900

    facebook 환경설정

--- app/src/main/java/com/example/tacademy/samplefacebook/MainActivity.java
+++ app/src/main/java/com/example/tacademy/samplefacebook/MainActivity.java
@@ -1,13 +1,44 @@
 package com.example.tacademy.samplefacebook;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
+public class MainActivity extends AppCompatActivity {
+    CallbackManager callbackManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        callbackManager = CallbackManager.Factory.create();
+        LoginButton btn=(LoginButton)findViewById(R.id.login_button);
+        btn.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 }
